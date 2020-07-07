a91221e00c374702c61044871d97c1c74d01a980
Devwrat <sdevwrat@gmail.com>
2020-07-07 21:38:03 +0530

    Updates

---
+++ precache-manifest.0442ef76281aa5504067b466c6fa09a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac527acf621f1b1ac6bc7c8cb19b3f6",
+    "url": "/React-Quiz/index.html"
+  },
+  {
+    "revision": "9c39967767063095b517",
+    "url": "/React-Quiz/static/css/main.5b212ec5.chunk.css"
+  },
+  {
+    "revision": "9971910fb621af793991",
+    "url": "/React-Quiz/static/js/2.945a072e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Quiz/static/js/2.945a072e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c39967767063095b517",
+    "url": "/React-Quiz/static/js/main.509e830d.chunk.js"
+  },
+  {
+    "revision": "48a2099ab6cecb8e4e72",
+    "url": "/React-Quiz/static/js/runtime-main.5b7f1df8.js"
+  }
+]);
